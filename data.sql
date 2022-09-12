da754eceafda881ab126cfc291fa3e8fc88b5aae
Hamma Azarok <hammaagazarok@gmail.com>
2022-09-12 15:23:56 +0200

    insert animals data

--- data.sql
+++ data.sql
@@ -1,5 +1,65 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    (
+        'Agumon',
+        TO_DATE('2020-02-03', 'YYYY-MM-DD'),
+        0,
+        true,
+        10.23
+    );
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    (
+        'Gabumon',
+        TO_DATE('2018-11-15', 'YYYY-MM-DD'),
+        2,
+        false,
+        8
+    );
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    (
+        'Pikachu',
+        TO_DATE('2021-1-7', 'YYYY-MM-DD'),
+        1,
+        false,
+        15.04
+    );        
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    (
+        'Devimon',
+        TO_DATE('2017-5-12', 'YYYY-MM-DD'),
+        5,
+        true,
+        11
+    );    
